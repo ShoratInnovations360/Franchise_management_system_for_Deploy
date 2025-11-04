16223e69ca0487a0d0d2bc3fd2f3145454aba859
ShoratInnovations360 <shoratinnovations@gmail.com>
2025-11-04 17:24:20 +0530

    Fix import path for staffmanagement in FranchiseLayout

--- frontend/shorat-connect/src/pages/Franchise/Layout/FranchiseLayout.jsx
+++ frontend/shorat-connect/src/pages/Franchise/Layout/FranchiseLayout.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import FranchiseHeader from "../Layout/FranchiseHeader";
 import { FranchiseSidebar } from "../Layout/FranchiseSidebar";
 import DashboardContent from "../Dashboard/DashboardContent";
-import StaffManagement from "../Franchise/staff/staffmanagement";
+import StaffManagement from "../staff/StaffManagement";
 import { NotificationPage } from "../Notifications/NotificationPage";
 import StudentManagement from "../Student/StudentManagement";
 import CourseManagement from "../course/CourseManagement";
