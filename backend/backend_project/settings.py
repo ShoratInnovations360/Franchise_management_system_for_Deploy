28aa525b2eabd66ea7262451d4cf685841effeab
ShoratInnovations360 <shoratinnovations@gmail.com>
2025-11-07 13:12:16 +0530

    email update.

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -162,8 +162,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "shoratteam@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "kimk scdg ntmv ffdb")
+
 
 # ── Security behind Render proxy ─────────────────────────────────
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
