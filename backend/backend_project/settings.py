b296157ed63cfe31af05a82ca615db86d7a26ce9
ShoratInnovations360 <shoratinnovations@gmail.com>
2025-12-15 14:15:44 +0530

    added

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "backend_project.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=0,
         ssl_require=True
     )
 }
