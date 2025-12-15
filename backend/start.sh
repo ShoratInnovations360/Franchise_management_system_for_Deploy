0503f0f9913d82bf4d2cae0a6b144cd9c796dbd5
ShoratInnovations360 <shoratinnovations@gmail.com>
2025-12-15 14:06:47 +0530

    added

--- backend/start.sh
+++ backend/start.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Waiting for database..."
-
-python << END
-import time, psycopg2, os
-
-db_url = os.environ["DATABASE_URL"]
-
-while True:
-    try:
-        psycopg2.connect(db_url)
-        print("Database is ready!")
-        break
-    except Exception as e:
-        print("Postgres unavailable - sleeping 5s")
-        time.sleep(5)
-END
-
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-
-exec gunicorn backend_project.wsgi:application --bind 0.0.0.0:$PORT
+echo "Starting Gunicorn..."
+gunicorn backend_project.wsgi:application --bind 0.0.0.0:$PORT
